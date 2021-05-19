e91519719e4969eb7258f220b9fcfff2a07b7f17
Viktor <morphey77@yandex.by>
2021-05-19 20:46:38 +0300

    initial

---
+++ src/viktor/com/Main.java
@@ -0,0 +1,11 @@
+package viktor.com;
+
+import viktor.com.lesson1.OperatorsDemo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(OperatorsDemo.isSeven(27));
+    }
+}
---
+++ src/viktor/com/lesson1/OperatorsDemo.java
@@ -0,0 +1,10 @@
+package viktor.com.lesson1;
+
+public class OperatorsDemo {
+    //HomeWork number 6
+    public static boolean isSeven(int number) {
+
+        return (number % 10) == 7;
+
+    }
+}
